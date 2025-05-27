45260b55f2f10e0ce825fcb90b6c161297c56645
david kim <dkreliable@gmail.com>
2025-05-27 09:36:20 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: '/auto-nova-website/', 
+  base: "/auto-nova-website/", 
   plugins: [react()]
   
 
