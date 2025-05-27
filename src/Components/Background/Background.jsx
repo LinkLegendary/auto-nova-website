d4cb889cdf5f1acb5e2b620fa8bbf18618b96c0f
david kim <dkreliable@gmail.com>
2025-05-27 11:20:16 -0400

    fix

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Background.css'
-import video1 from '../../Assets/video1.mp4'
-import image1 from '../../Assets/image1.png'
-import image2 from '../../Assets/image2.png'
-import image3 from '../../Assets/image3.png'
+import video1 from '../Assets/video1.mp4'
+import image1 from '../Assets/image1.png'
+import image2 from '../Assets/image2.png'
+import image3 from '../Assets/image3.png'
 const Background = ({heroCount, playStatus}) => {
 
    if(playStatus){
